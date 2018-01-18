ae4db63aa85686e5ecec97a5ae41687a12f6cf52
Felana <felana.r@hotmail.fr>
2018-01-18 22:55:59 +0100

    Design de la page des données de l'utilisateur

--- e-boutique/views/infosPersos.php
+++ e-boutique/views/infosPersos.php
@@ -1,1 +1,29 @@
-<?php echo "Infos personelles"; ?>
+<div class="container">
+  <form>
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label for="inputEmail4">Email</label>
+        <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+      </div>
+      <div class="form-group col-md-6">
+        <label for="inputPassword4">Nouveau mot de passe</label>
+        <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+      </div>
+      <div class="form-group col-md-6">
+        <label for="inputEmail4">Adresse</label>
+        <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+      </div>
+      <div class="form-group col-md-6">
+        <label for="inputPassword4">Code postal</label>
+        <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+      </div>
+      <div class="form-group col-md-6">
+        <label for="inputPassword4">Ville</label>
+        <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+      </div>
+    </div>
+    <div class="form-group col-md-6">
+      <button type="submit" class="btn btn-primary">Enregistrer les modifications</button>
+  </div>
+  </form>
+</div>
