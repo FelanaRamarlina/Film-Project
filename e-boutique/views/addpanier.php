27b7d40e991542e52129af40b53877795c39c242
thenear95 <nicolas.sabak@gmail.com>
2018-01-21 17:24:52 +0100

    Ajout d'un film en session

--- e-boutique/views/addpanier.php
+++ e-boutique/views/addpanier.php
@@ -1,4 +1,10 @@
 <?php 
 require 'index.php';
 require './model/Connexion.class.php';
+
+if(isset($_GET['id_produit'])){
+    $product = $db->query('SELECT id_produit FROM produit WHERE id_produit=:id_produit', array('id' => $_GET['id']));
+}else{
+    die("Vous n'avez pas séléctionné de produit à ajouter dans votre panier");
+}
 ?>
